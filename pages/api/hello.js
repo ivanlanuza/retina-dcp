5e936f736c9840923f17454355016a25148265dc
isles-isles <jisles@iripple.com>
2025-02-07 04:30:40 +0800

    deleted hello file (#11)
    Co-authored-by: “jisles” <“jisles@“iripple.com>

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-import { protectAPI } from "@/utils/auth";
-
-export default protectAPI(async (req, res) => {
-  // Your protected route logic
-  res.status(200).json({
-    message: "Protected data",
-    user: req.user,
-  });
-});
