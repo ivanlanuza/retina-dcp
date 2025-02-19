0156ff8a9f9a607f7ad55d921498770d4ac29ebb
icdlanuza <icdlanuza@gmail.com>
2025-02-19 08:46:53 +0800

    survey list

--- pages/tools/surveys.js
+++ pages/tools/surveys.js
@@ -1,13 +1,55 @@
+import { useEffect, useState } from "react";
 import Layout from "@/components/core/Layout";
+import withAuth from "@/components/core/Auth";
+import SurveyDataTable from "@/components/tools/survey/SurveyDataTable";
 
-export default function Dashboard() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [surveys, setSurveys] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    fetch("/api/survey/admin/getSurveyList", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSurveys(data);
+        setIsLoading(false);
+      });
+  }, [isLoading]);
+
+  if (isLoading) {
+    return (
+      <Layout>
+        <div className="mx-8 font-sans">
+          <h2 className="text-2xl font-bold font-sans text-gray-800 mb-6">
+            Surveys
+          </h2>
+        </div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <div className="max-w-4xl mx-auto">
-        <h2 className="text-2xl font-bold font-sans text-gray-800 mb-6">
+      <div className="mx-8 font-sans text-sm">
+        <h2 className="text-2xl font-bold font-sans text-gray-800 mb-12">
           Surveys
         </h2>
+        {surveys && (
+          <SurveyDataTable
+            surveydata={surveys}
+            onSave={(e) => setIsLoading(true)}
+          />
+        )}
       </div>
     </Layout>
   );
-}
+};
+
+export default withAuth(App);
