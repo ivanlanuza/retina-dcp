5e770ccc0407295eb5715ce74a40401ea2b1af0a
icdlanuza <icdlanuza@gmail.com>
2025-03-05 10:06:10 +0800

    Update Dashboards

--- components/core/Dashboard.js
+++ components/core/Dashboard.js
@@ -1,31 +1,140 @@
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-//import { BarChart, LineChart } from "recharts";
+import { useState, useEffect } from "react";
 import { BarChart, LineChart } from "@/components/ui/chart";
-import { Users, MapPin, Building } from "lucide-react";
+import { Users, MapPin, PackageSearch } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function Dashboard() {
+  const [locations, setLocations] = useState({ loading: true, data: [] });
+  const [users, setUsers] = useState({ loading: true, data: [] });
+  const [products, setProducts] = useState({ loading: true, data: [] });
+  const [oos, setOOS] = useState({ loading: true, data: [] });
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    fetch("/api/locations/getCountLocations", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLocations({ data: data, loading: false });
+      });
+
+    fetch("/api/users/getCountUsers", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers({ data: data, loading: false });
+      });
+
+    fetch("/api/masterdata/products/getCountProducts", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts({ data: data, loading: false });
+      });
+
+    fetch(
+      "/api/locations/getLocationsOOS?" +
+        new URLSearchParams({
+          limit: 10,
+        }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setOOS({ data: data, loading: false });
+      });
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen font-sans">
       <main className="flex-grow p-6">
         <div className="grid gap-4 md:grid-cols-3 mb-8">
           <StatCard
             title="Locations Covered"
-            value="1,234"
+            value={locations.loading ? "..." : locations.data}
             icon={<MapPin className="h-6 w-6" />}
           />
           <StatCard
             title="Active Users"
-            value="5,678"
+            value={users.loading ? "..." : users.data}
             icon={<Users className="h-6 w-6" />}
           />
           <StatCard
-            title="Covered Customers"
-            value="126"
-            icon={<Building className="h-6 w-6" />}
+            title="Active Products"
+            value={products.loading ? "..." : products.data}
+            icon={<PackageSearch className="h-6 w-6" />}
           />
         </div>
+
+        <div className="grid gap-4 md:grid-cols-3 mb-8">
+          <div className="bg-white border border-gray-200 p-4 rounded-xl shadow-md">
+            <h2 className="text-sm font-medium text-gray-500 mt-2">
+              Locations with most Out-of-Stock SKUs
+            </h2>
+            <div>
+              <Table className="text-xs text-gray-400 -mx-2 mt-6">
+                <TableHeader>
+                  <TableRow className="">
+                    <TableHead className="text-left text-indigo-800">
+                      Location
+                    </TableHead>
+                    <TableHead className="text-center text-indigo-800">
+                      OOS SKU Count
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {oos.loading ? (
+                    <tr>
+                      <td colSpan="2">Loading...</td>
+                    </tr>
+                  ) : (
+                    oos.data.map((location, index) => (
+                      <TableRow key={index}>
+                        <TableCell className="text-left">
+                          {location.name}
+                        </TableCell>
+                        <TableCell className="text-center">
+                          {location.count}
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </TableBody>
+              </Table>
+            </div>
+          </div>
+        </div>
+
+        {/* 
         <div className="grid gap-6 md:grid-cols-2">
           <ChartCard title="Share of Shelf" chart={<BarChart />} />
           <ChartCard title="In-Store Availability" chart={<BarChart />} />
@@ -34,6 +143,7 @@ export default function Dashboard() {
           <ChartCard title="Sales by Category" chart={<BarChart />} />
           <ChartCard title="Customer Satisfaction" chart={<LineChart />} />
         </div>
+        */}
       </main>
     </div>
   );
---
+++ pages/api/users/getCountUsers.js
@@ -0,0 +1,41 @@
+import { SystemResponse } from "../../../utils/backend/response";
+import { validateToken } from "../../../utils/backend/middleware";
+import { PrismaClient } from "@prisma/client";
+import { jwtDecode } from "jwt-decode";
+
+const prisma = new PrismaClient();
+const response = new SystemResponse();
+
+export default async function handler(req, res) {
+  const isTokenValid = validateToken(req, res);
+  if (!isTokenValid) return;
+
+  const accountid = parseInt(
+    jwtDecode(req.headers.authorization.split(" ")[1]).accountId
+  );
+
+  if (req.method === "GET") {
+    try {
+      const count = await prisma.users.count({
+        where: {
+          accountId: accountid,
+          isdeleted: false,
+        },
+      });
+
+      //console.log(locations);
+      res.status(200).json(count);
+      return;
+    } catch (error) {
+      console.error(error);
+      return response.getFailedResponse(res, 500, {
+        message: "Error retrieving data",
+        error: error.message,
+      });
+    }
+  } else {
+    return response.getFailedResponse(res, 405, {
+      message: "Method Not Allowed",
+    });
+  }
+}
